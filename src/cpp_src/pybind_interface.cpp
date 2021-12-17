0939ef16ea96f0626076c0a6ac46a68a9df816bb
Oliver Thim <toliver@elan.local>
2021-12-17 10:13:27 +0100

    Now adding features to the python interface

--- src/cpp_src/pybind_interface.cpp
+++ src/cpp_src/pybind_interface.cpp
@@ -1,14 +1,81 @@
 #include "pybind_interface.h"
 
-nn_mwpc_interface::nn_mwpc_interface(const std::string& model_name, double scale_gl_grid, 
-        int gl_grid_size, int ang_int_size, int J_max_in_pot, int J_max_chn)
+nn_mwpc_interface::nn_mwpc_interface(const std::string& model_name, 
+        int J_max_chn, double cutoff, bool pre_comp_pot, bool rel_corr)
 {
-    // Construct potential
+
+    // ------ CONSTANTS TO CHANGE ------
+    // ---------------------------------
+    double scale_ = 100.0; // Scale of momenutm grid MeV
+    int number_of_p_points_ = 100; // Number of momentum-grid points
+    int ang_int_points_ = 96; // Number of points in angular integration
+    int J_max_in_pot_ = 50; // Maximum J that is stored for L-polynomials
+    double cutoff_ = cutoff; // Cutoff in LS-equation
+    bool pre_comp_pot_ = pre_comp_pot; // If pre-computations should be made
+    bool rel_corr_ = rel_corr;
+
+    // For the quantum states
+    int J_max = J_max_chn;
+    int J_min = 0;
+    int Tz_min = 0;
+    int Tz_max = 0;
+    bool print = true;
+    // ---------------------------------
+    // ---------------------------------
+
+    // Initialize physics helpers
+    ph::physics_helper_init();
+
+    // Construct the quantum states and quantum channels
+    std::vector<qs::quantum_NN_state> states = get_states_NN(J_max, J_min, 
+            Tz_min, Tz_max, print);
+     
+    // Construct the quantum scattering channels from the states
+    chns_ = get_channels(states, print);   
+
+    // Make GL grid
+    double* p_grid;
+    double* w_grid;
+    ph::gauss_legendre_inf_mesh(number_of_p_points_,scale_,&p_grid,&w_grid);
     
-    // Construct LS Solver
-    
-    // Make pre-computations
+    // These are the pre-determined models
+    if ("WPC_LO"==model_name)
+    {
+        // Choose terms in potential
+        std::vector<std::string terms> terms;
+        terms.push_back("OPEP");
+        terms.push_back("C1S0");
+        terms.push_back("C3S1");
+
+        // Construct potential
+        Pot_ = new Potential_mwpc(terms,ang_int_points_,p_grid,w_grid,
+                number_of_p_points_,J_max_in_pot_,cutoff_);
+        Pot_ext_ = nullptr;
+
+        if (pre_comp_pot)
+        {
+            // Save potential
+            for (auto chn : chns_)
+            {
+                Pot->populate_saved_mtx(chn,rel_corr_); // Realtivistic factor on
+            }
+        }
+
+        // Construct LS Solver
+        LS_Solver_ = new LS_Solver(chns_,number_of_p_points_, scale_
+                cutoff_,rel_corr_);
+
+    } else
+    {
+        std::cout << "Error, not a valid model_name" << std::endl;
+    }
 }
+
+nn_mwpc_interface::initialize()
+{
+    // Delete all pointers
+}
+
 nn_mwpc_interface::~nn_mwpc_interface()
 {
 
@@ -21,8 +88,10 @@ std::vector<double> nn_mwpc_interface::compute_observable(const std::string& nam
     
     // For the specified angles (not 90...) 
     std::vector<double> obs_ang;
-    obs_ang.push_back(10.0);
-    obs_ang.push_back(20.0);
+    /*for (int i = 0; i < 1000; i++)
+    {
+        obs_ang.push_back((double)i);
+    }*/
 
     return obs_ang;
 }
--- src/cpp_src/pybind_interface.h
+++ src/cpp_src/pybind_interface.h
@@ -58,14 +58,16 @@ private:
     int J_max_in_pot_;   
 
     // Objects 
-    //Potential_mwpc Pot_;
-    //Potential_ext Pot_ext_;
+    Potential_mwpc* Pot_;
+    Potential_ext* Pot_ext_;
 
-    //LS_Solver LS_Solver_;
+    LS_Solver* LS_Solver_;
 
     std::vector<qs::quantum_channel> Chns_;
 public:
-    nn_mwpc_interface(const std::string& model_name, double scale_gl_grid, int gl_grid_size, int ang_int_size, int J_max_in_pot, int J_max_chn);
+    nn_mwpc_interface(const std::string& model_name, double scale_gl_grid, 
+            int gl_grid_size, int ang_int_size, int J_max_in_pot, int J_max_chn, 
+            bool pre_comp_pot);
     ~nn_mwpc_interface();
     
     std::vector<double> compute_observable(const std::string& name, 
@@ -78,11 +80,12 @@ public:
 
 namespace py = pybind11;
 
-
 PYBIND11_MODULE(nn_mwpc, m) 
 {
     py::class_<nn_mwpc_interface>(m,"nn_mwpc_interface")
         .def(py::init<const std::string&,double,int,int,int,int>())
-        .def("compute_observable", &nn_mwpc_interface::compute_observable,py::return_value_policy::copy);
+        .def("compute_observable", &nn_mwpc_interface::compute_observable,
+                py::return_value_policy::copy);
 }
+
 #endif
