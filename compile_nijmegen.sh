2deb83d50dcebca325ab3a154599d005104e23be
Oliver Thim <toliver@zensei>
2022-01-28 15:14:06 +0100

    Automated compilation

---
+++ compile_nijmegen.sh
@@ -0,0 +1,11 @@
+
+
+(cd nijm-pot-working-cpp-class; make cleanall)
+
+(cd nijm-pot-working-cpp-class; make)
+
+(cd nijm-pot-working-cpp-class; cp nijmegen/nijmegen_interface.o ../src/cpp_src)
+(cd nijm-pot-working-cpp-class; cp nijmegen/pnijm.o ../src/cpp_src)
+
+
+
