84a5ca51f3e226d95b470d938564e18432e45453
Oliver Thim <toliver@zensei>
2022-04-06 13:47:50 +0200

    Added some files to version controle and updated the .gitignore

---
+++ compile_cdbonn.sh
@@ -0,0 +1,8 @@
+
+(cd cdbonn; make cleanall)
+
+(cd cdbonn; make)
+
+(cd cdbonn; cp cdbonn_interface.o ../src/cpp_src)
+(cd cdbonn; cp cdbonn.o ../src/cpp_src)
+
---
+++ export.sh
@@ -0,0 +1,6 @@
+export LD_PRELOAD=/net/opt/intel/2022.1.2.146/intel/oneapi/mkl/2022.0.2/lib/intel64/libmkl_def.so.2:/net/opt/intel/2022.1.2.146/intel/oneapi/mkl/2022.0.2/lib/intel64/libmkl_avx2.so.2:
+/net/opt/intel/2022.1.2.146/intel/oneapi/mkl/2022.0.2/lib/intel64/libmkl_core.so:/net/opt/intel/2022.1.2.146/intel/oneapi/mkl/2022.0.2/lib/intel64/libmkl_intel_lp64.so:/net/opt/intel/2022.1.2.146/intel/oneapi/mkl/2022.0.2/lib/intel64/libmkl_intel_thread.so:/net/opt/intel/lib/intel64/libiomp5.so
+
+
+
+# libmkl_avx2.so.2 or libmkl_def.so.2.
