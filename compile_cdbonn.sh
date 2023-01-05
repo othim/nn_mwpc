d0a70c80e2adeb9eb952a45ea79e30773319f471
Oliver Thim <oliver.thim@chalmers.se>
2023-01-05 14:59:15 +0100

    Rearranged in the repo

--- compile_cdbonn.sh
+++ compile_cdbonn.sh
@@ -1,8 +1,8 @@
 
-(cd cdbonn; make cleanall)
+(cd src/potential_codes/cdbonn; make cleanall)
 
-(cd cdbonn; make)
+(cd src/potential_codes/cdbonn; make)
 
-(cd cdbonn; cp cdbonn_interface.o ../src/cpp_src)
-(cd cdbonn; cp cdbonn.o ../src/cpp_src)
+(cd src/potential_codes/cdbonn; cp cdbonn_interface.o ../cpp_src)
+(cd src/potential_codes/cdbonn; cp cdbonn.o ../cpp_src)
 
--- compile_nijmegen.sh
+++ compile_nijmegen.sh
@@ -1,11 +1,11 @@
 
 
-(cd nijm-pot-working-cpp-class; make cleanall)
+(cd src/potential_codes/nijm-pot-working-cpp-class; make cleanall)
 
-(cd nijm-pot-working-cpp-class; make)
+(cd src/potential_codes/nijm-pot-working-cpp-class; make)
 
-(cd nijm-pot-working-cpp-class; cp nijmegen/nijmegen_interface.o ../src/cpp_src)
-(cd nijm-pot-working-cpp-class; cp nijmegen/pnijm.o ../src/cpp_src)
+(cd src/potential_codes/nijm-pot-working-cpp-class; cp nijmegen/nijmegen_interface.o ../cpp_src)
+(cd src/potential_codes/nijm-pot-working-cpp-class; cp nijmegen/pnijm.o ../cpp_src)
 
 
 
