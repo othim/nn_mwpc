f9c639d6115b32f6024ba14a076ff45e80ca5cff
Oliver Thim <oliver.thim@chalmers.se>
2025-05-12 19:27:53 +0200

    Added preload in compile.sh

--- test_code/compile.sh
+++ test_code/compile.sh
@@ -2,3 +2,7 @@
 (cd ../src/cpp_src; make -j so)
 
 cp ../src/cpp_src/libnn_mwpc.so .
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/chalmers/users/toliver/phd/nn_mwpc/test_code
+
+export LD_PRELOAD=$LD_PRELOAD:/chalmers/users/toliver/phd/nn_mwpc/test_code/nn_mwpc.so
